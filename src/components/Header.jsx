74db2e1c3706bc29c948a28c8c29351eb0e2fffb
M.DEEPAN <100012988+Deepan369@users.noreply.github.com>
2024-08-06 12:22:37 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightTwoToneIcon from "@mui/icons-material/HighlightTwoTone";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <HighlightTwoToneIcon />
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
